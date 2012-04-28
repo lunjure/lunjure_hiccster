be4a9c05a41dfe56fa9f83e9a65ca475226140d3
Gregor Adams <greg@pixelass.de>
2012-04-28 21:48:18 +0200

    added scripts

---
+++ static/js/greg.js
@@ -0,0 +1,32 @@
+(function($) {
+    $(document).ready(function() {
+      var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+       ];
+
+       $( "#message" ).autocomplete({
+        source: availableTags
+       });
+    });
+})(jQuery);
