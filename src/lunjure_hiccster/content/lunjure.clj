f276cc0c935348b14fa921e171ebec31c51172e6
Gregor Adams <greg@pixelass.de>
2012-04-28 20:39:12 +0200

    changed attribute usage and added styles

--- src/lunjure_hiccster/content/lunjure.clj
+++ src/lunjure_hiccster/content/lunjure.clj
@@ -13,21 +13,32 @@
         [:div#text_pad 
          [:div.wrapper
          (when dummy?
+          (list
           [:ul.team
            [:li.team-name "Havanna"
            [:ul.users 
             [:li.user "Daniel"]
-            [:li.user "Moritz"]]]])]]
+            [:li.user "Moritz"]]]]
+          [:ul.team
+           [:li.team-name "REWE"
+           [:ul.users 
+            [:li.user "Gregor"]
+            [:li.user "Guido"]
+            [:li.user "Gerrit"]]]]
+            ))]]
         [:section#chat_window 
          [:div#text_window
           [:div.output
           (when dummy?
            (list
-            [:p {:data-username "moritz" :data-usercolor "red"} 
+            [:p {:name "moritz" :type "red" :alt "04:20"} 
              "I'm really hungry "
             [:span.emoticon {:data-emoticon "lol"}]]
-            [:p {:data-username "gregor" :data-usercolor "orange"} 
-             "duuhh "
+            [:p {:name "Guido" :type "yellow" :alt "04:20"} 
+             "does every name have to be 6 characters long? "
+            [:span.emoticon {:type "blink"}]]
+            [:p {:name "gregor" :type "orange" :alt "04:21"} 
+             "no..duuhh "
             [:span.emoticon {:data-emoticon "wtf"}]
             [:span.emoticon {:data-emoticon "smile"}]
             [:span.emoticon {:data-emoticon "oo"}]
@@ -35,9 +46,9 @@
             [:span.emoticon {:data-emoticon "blinktongue"}]
             [:span.emoticon {:data-emoticon "coffee"}]
             [:span.emoticon {:data-emoticon "sun"}]
-             " me too"]
-            [:p {:data-username "daniel" :data-usercolor "yellow"} "created team Havanna "]))]
+             " and we can also use multiple emoticons"]
+            [:p.status "daniel created team Havanna "]))]
           [:section.input-field
-           [:div {:contenteditable "true" :id "message" :name "message"}]]]
+           [:input {:type "text" :id "message" :name "message"}]]]
 
            ]]])))
