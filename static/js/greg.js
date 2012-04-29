ac916c09a650aae61f22ddcd4d3fef8d4ca63f22
Gregor Adams <greg@pixelass.de>
2012-04-29 18:41:26 +0200

    autocomplete suggestions use food-places

--- static/js/greg.js
+++ static/js/greg.js
@@ -1,40 +1,25 @@
 (function($) {
-    $(document).ready(function() {
-      var availableTags = [
-        "ActionScript",
-        "AppleScript",
-        "Asp",
-        "BASIC",
-        "C",
-        "C++",
-        "Clojure",
-        "COBOL",
-        "ColdFusion",
-        "Erlang",
-        "Fortran",
-        "Groovy",
-        "Haskell",
-        "Java",
-        "JavaScript",
-        "Lisp",
-        "Perl",
-        "PHP",
-        "Python",
-        "Ruby",
-        "Scala",
-        "Scheme"
-       ];
-
-       $( "#message" ).autocomplete({
-        source: availableTags
-       });
-
-var newP = '<p class="status" data-time="04:30">someone created something</p>',
-    scrollSection = $('#text_window .output');
- $('#testbutton').click(function(){
-  scrollSection.append(newP);
- scrollSection.scrollTop(scrollSection[0].scrollHeight);
- });
-
-    });
+  $(document).ready(function() {
+    var availableTags = [
+      "REWE",
+      "mcDonalds",
+      "Burgerking",
+      "Bagel brothers",
+      "Havanna",
+      "Kugelfisch",
+      "Ichiban",
+      "Tuscolo",
+      "Maredo",
+      "Penny",
+     ];
+     $( "#message" ).autocomplete({
+      source: availableTags
+     });
+     var newP = '<p class="status" data-time="04:30">someone created something</p>',
+         scrollSection = $('#text_window .output');
+     $('#testbutton').click(function(){
+         scrollSection.append(newP);
+         scrollSection.scrollTop(scrollSection[0].scrollHeight);
+      });
+  });
 })(jQuery);
